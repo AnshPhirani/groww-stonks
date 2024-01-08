15b2642b1531a9a5292a9e1f3534179abcfe39b1
Ansh Phirani <anshphirani@gmail.com>
2024-01-09 01:54:09 +0530

    removed base path (added by mistake)

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
   images: {
     domains: ["api-ninjas-data.s3.us-west-2.amazonaws.com", "api.polygon.io"],
   },
-  basePath: "/soom-homes",
 };
 
 module.exports = nextConfig;

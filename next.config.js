3b6c9fe507c1f209aa16d9de14f104e916e921c3
Ansh Phirani <anshphirani@gmail.com>
2024-01-09 01:52:45 +0530

    updated basepath

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   images: {
     domains: ["api-ninjas-data.s3.us-west-2.amazonaws.com", "api.polygon.io"],
   },
+  basePath: "/soom-homes",
 };
 
 module.exports = nextConfig;
